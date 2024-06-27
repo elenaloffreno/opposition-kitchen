96df6131c6a20e0149c022a4214cd34807dd0445
elenaloffreno <loffrenoelena@gmail.com>
2024-06-27 14:01:30 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let cursor = document.getElementById("cursor");
+let size;
+document.body.addEventListener("mousemove", (ev)=>{
+    let path = ev.composedPath();
+    
+    if (path.some(x=>x.tagName == "A")) size = 20;
+    else if (path.some(x=>x.tagName == "BUTTON")) size = 20; // adjust size for <button> tag
+    
+    else size = 10; // default size if not over <a> or <button>
+    
+    cursor.style.left   = (ev.clientX - size/2) + "px";
+    cursor.style.top    = (ev.clientY - size/2) + "px";
+    cursor.style.width  = size + "px";
+    cursor.style.height = size + "px";
+});
